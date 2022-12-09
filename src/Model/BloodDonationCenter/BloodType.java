945907d5c8b0f5c069f9d3185864c53c97ee980d
Devika Boddu <boddu.d@northeastern.edu>
2022-12-09 18:22:37 -0500

    added changes

--- src/Model/BloodDonationCenter/BloodType.java
+++ src/Model/BloodDonationCenter/BloodType.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Model.BloodDonationCenter;
-//added changes
+//added changes to BloodDonationCenter
 /**
  *
  * @author devikaboddu
