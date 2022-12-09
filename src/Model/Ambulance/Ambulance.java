6a00c135cfe47aac2cfa93afe0c4656b6c009e4a
Devika Boddu <boddu.d@northeastern.edu>
2022-12-09 17:59:50 -0500

    added Laboratory model

--- src/Model/Ambulance/Ambulance.java
+++ src/Model/Ambulance/Ambulance.java
@@ -4,10 +4,99 @@
  */
 package Model.Ambulance;
 
+import Model.Patient.Patient;
+import java.util.ArrayList;
+
+
 /**
  *
  * @author devikaboddu
  */
 public class Ambulance {
     
+    private String ambulanceDriverFirstName;
+    private String ambulanceDriverLastName;
+    private String ambulanceVehicleNo;
+    private String ambulanceDriverPhoneNo;
+    private String ambulanceDriverStatus;
+    private int ambulanceDriverAge;
+    private String accidentPlace;
+    private ArrayList<Patient> ambulancePatientsList;
+    
+    public Ambulance()
+    {
+         this.ambulancePatientsList = new ArrayList<Patient>();
+    }
+
+    public String getDriverFirstName() {
+        return ambulanceDriverFirstName;
+    }
+
+    public void setDriverFirstName(String driverFirstName) {
+        this.ambulanceDriverFirstName = driverFirstName;
+    }
+
+    public String getDriverLastName() {
+        return ambulanceDriverLastName;
+    }
+
+    public void setDriverLastName(String driverLastName) {
+        this.ambulanceDriverLastName = driverLastName;
+    }
+
+    public String getVehicleNo() {
+        return ambulanceVehicleNo;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        this.ambulanceVehicleNo = vehicleNo;
+    }
+
+    public String getDriverPhoneNo() {
+        return ambulanceDriverPhoneNo;
+    }
+
+    public void setDriverPhoneNo(String driverPhoneNo) {
+        this.ambulanceDriverPhoneNo = driverPhoneNo;
+    }
+
+    public String getStatus() {
+        return ambulanceDriverStatus;
+    }
+
+    public void setStatus(String Status) {
+        this.ambulanceDriverStatus = Status;
+    }
+
+    public int getDriverAge() {
+        return ambulanceDriverAge;
+    }
+
+    public void setDriverAge(int driverAge) {
+        this.ambulanceDriverAge = driverAge;
+    }
+
+    public String getAccidentLocation() {
+        return accidentPlace;
+    }
+
+    public void setAccidentLocation(String accidentLocation) {
+        this.accidentPlace = accidentLocation;
+    }
+
+    public ArrayList<Patient> getAmbulancePatientsList() {
+        return ambulancePatientsList;
+    }
+
+    public void setAmbulancePatientsList(ArrayList<Patient> ambulancePatientsList) {
+        this.ambulancePatientsList = ambulancePatientsList;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return ambulanceDriverFirstName;
+    }
 }
+
--- src/Model/Ambulance/AmbulanceDirectory.java
+++ src/Model/Ambulance/AmbulanceDirectory.java
@@ -4,10 +4,31 @@
  */
 package Model.Ambulance;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author devikaboddu
  */
 public class AmbulanceDirectory {
-    
+    private ArrayList<Ambulance> ambulancesList;
+
+    public ArrayList<Ambulance> getAmbulances() {
+        return ambulancesList;
+    }
+
+    public void setAmbulances(ArrayList<Ambulance> Ambulances) {
+        this.ambulancesList = Ambulances;
+    }
+
+    public AmbulanceDirectory()
+    {
+         ambulancesList = new ArrayList<>();
+    }
+     
+    public Ambulance createAmbulance(Ambulance ambulance) 
+    {
+        ambulancesList.add(ambulance);
+        return ambulance;
+    }
 }
