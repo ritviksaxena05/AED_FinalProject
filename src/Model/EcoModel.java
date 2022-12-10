9ab3ad0a2ab41db1ce55aa652c44b4b042949d63
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2022-12-09 19:13:00 -0500

    Medicine Request by Doctor panel

--- src/Model/EcoModel.java
+++ src/Model/EcoModel.java
@@ -5,6 +5,7 @@
 package Model;
 
 import Model.Doctor.DoctorDirectory;
+import Model.Pharmacy.Pharmacy;
 import Model.Roles.Role;
 import java.util.ArrayList;
 
@@ -15,6 +16,15 @@ import java.util.ArrayList;
 public class EcoModel extends OrganizationModel {
     private static EcoModel business;
     private DoctorDirectory doctorDirectory;
+    private Pharmacy pharmacy;
+
+    public Pharmacy getPharmacy() {
+        return pharmacy;
+    }
+
+    public void setPharmacy(Pharmacy pharmacy) {
+        this.pharmacy = pharmacy;
+    }
 
     public static EcoModel getBusiness() {
         return business;
